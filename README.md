18a31147053fe030f4e8b9c1c4e3f34bc54c4653
evanyui <evanyui@hotmail.com>
2018-01-21 05:08:58 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+A weather forecast on terminal
+
+A fun mini project for python beginners:
+File I/O
+Using API
+if\elif\else statements
+
+Included ASCII arts of weather icon
+
+Easy and pretty
---
+++ weather.py
@@ -0,0 +1,38 @@
+import json, urllib.request 
+
+f = open('clear.txt','r')
+clear = f.read()
+f.close()
+f = open('clouds.txt','r')
+clouds = f.read()
+f.close()
+f = open('drizzle.txt','r')
+drizzle = f.read()
+f.close()
+f = open('rain.txt','r')
+rain = f.read()
+f.close()
+f = open('default.txt','r')
+default = f.read()
+f.close()
+
+URL = "http://api.openweathermap.org/data/2.5/weather?zip=30318,us&units=metric&appid=d54cd01e8005bc84825fce37ff2d073a"
+req = urllib.request.Request(URL)
+response = urllib.request.urlopen(req).read()
+data = json.loads(response)
+
+print('\033[1;32;49m\nToday\'s weather:')
+weather = str(data['weather'][0]['main'])
+if weather == 'Clear':
+    print('\033[1;33;49m\n' + clear)
+elif weather == 'Clouds':
+    print('\033[1;33;49m\n' + clouds)
+elif weather == 'Drizzle':
+    print('\033[1;34;49m\n' + drizzle)
+elif weather == 'Rain' or weather == 'Snow':
+    print('\033[1;34;49m\n' + rain)
+else:
+    print('\033[1;34;49m\n' + default)
+
+print('\033[1;32;49m\t\t' + str(int(data['main']['temp'])) + '°C', '|', str(data['weather'][0]['description']))
+print('\t  min:', str(data['main']['temp_min']) + '°C', '| max:', str(data['main']['temp_max']) + '°C\n')
