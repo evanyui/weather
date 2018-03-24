b04e3d7da0f569bddbcd6bcda89fb6500850a5d7
evanyui <evanyui@hotmail.com>
2018-03-24 18:18:47 -0400

    catch no internet connection error

--- weather.py
+++ weather.py
@@ -16,23 +16,27 @@ f = open(os.path.dirname(__file__)+'/default.txt','r')
 default = f.read()
 f.close()
 
-URL = "http://api.openweathermap.org/data/2.5/weather?zip=30318,us&units=metric&appid=d54cd01e8005bc84825fce37ff2d073a"
-req = urllib.request.Request(URL)
-response = urllib.request.urlopen(req).read()
-data = json.loads(response)
+try: 
+    URL = "http://api.openweathermap.org/data/2.5/weather?zip=30318,us&units=metric&appid=d54cd01e8005bc84825fce37ff2d073a"
+    req = urllib.request.Request(URL)
+    response = urllib.request.urlopen(req).read()
+    data = json.loads(response)
 
-print('\033[1;32;49m\nToday\'s weather:')
-weather = str(data['weather'][0]['main'])
-if weather == 'Clear':
-    print('\033[1;33;49m\n' + clear)
-elif weather == 'Clouds':
-    print('\033[1;33;49m\n' + clouds)
-elif weather == 'Drizzle':
-    print('\033[1;34;49m\n' + drizzle)
-elif weather == 'Rain' or weather == 'Snow':
-    print('\033[1;34;49m\n' + rain)
-else:
-    print('\033[1;34;49m\n' + default)
+    print('\033[1;32;49m\nToday\'s weather:')
+    weather = str(data['weather'][0]['main'])
+    if weather == 'Clear':
+        print('\033[1;33;49m\n' + clear)
+    elif weather == 'Clouds':
+        print('\033[1;33;49m\n' + clouds)
+    elif weather == 'Drizzle':
+        print('\033[1;34;49m\n' + drizzle)
+    elif weather == 'Rain' or weather == 'Snow':
+        print('\033[1;34;49m\n' + rain)
+    else:
+        print('\033[1;34;49m\n' + default)
 
-print('\033[1;32;49m\t\t' + str(int(data['main']['temp'])) + '°C', '|', str(data['weather'][0]['description']))
-print('\t   min:', str(data['main']['temp_min']) + '°C', '| max:', str(data['main']['temp_max']) + '°C\n')
+    print('\033[1;32;49m\t\t' + str(int(data['main']['temp'])) + '°C', '|', str(data['weather'][0]['description']))
+    print('\t   min:', str(data['main']['temp_min']) + '°C', '| max:', str(data['main']['temp_max']) + '°C\n')
+
+except:
+    print('No internet connection')
